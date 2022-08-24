1844ef6d68833308cda889587c8228bf8a695f8b
lailabougria <laila.bougria@particular.net>
2022-08-24 13:36:47 +0200

    Add sample

---
+++ message-processing-failed-but-whats-the-root-cause/sample/src/Sales/OrderChargedHandler.cs
@@ -0,0 +1,21 @@
+﻿using Events;
+using NServiceBus;
+using NServiceBus.Logging;
+
+namespace Sales;
+
+public class OrderChargedHandler : IHandleMessages<IOrderCharged>
+{
+    private static readonly ILog log = LogManager.GetLogger<OrderChargedHandler>();
+
+    public async Task Handle(IOrderCharged message, IMessageHandlerContext context)
+    {
+        log.Info($"Order '{message.Order.OrderId}' was placed.");
+
+        await context.Publish<IOrderPlaced>(orderPlaced =>
+        {
+            orderPlaced.CustomerId = message.CustomerId;
+            orderPlaced.Order = message.Order;
+        });
+    }
+}
